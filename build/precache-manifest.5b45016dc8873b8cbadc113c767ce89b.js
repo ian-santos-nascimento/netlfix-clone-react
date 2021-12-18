059ee209c1c50abe25a6a70bd406e09b468d5433
Ian Santos <ian290303@gmail.com>
2021-12-18 13:32:13 -0400

    Tentativa de psuh de gh pages

---
+++ build/precache-manifest.5b45016dc8873b8cbadc113c767ce89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f2775c104296c8f9cb252063aebf60",
+    "url": "/netlfix-clone-react/index.html"
+  },
+  {
+    "revision": "7a17409e503561adac3c",
+    "url": "/netlfix-clone-react/static/css/main.68fcb966.chunk.css"
+  },
+  {
+    "revision": "d0937997ef2c4d8f16a1",
+    "url": "/netlfix-clone-react/static/js/2.13f547ef.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/netlfix-clone-react/static/js/2.13f547ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a17409e503561adac3c",
+    "url": "/netlfix-clone-react/static/js/main.40c95bc9.chunk.js"
+  },
+  {
+    "revision": "c6603dc57d59037b1cca",
+    "url": "/netlfix-clone-react/static/js/runtime-main.f7af0f03.js"
+  }
+]);
